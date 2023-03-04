c3efea0a3827952fcb1cdd73b4cf988c031fff43
deirdre <deirdre.loyall@gmail.com>
2023-03-04 17:22:34 -0500

    basic search term functionality added

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HiOutlineSearch } from 'react-icons/hi';
+import './SearchBar.css';
 
 const SearchBar = (props) => {
+    const [ingredients, setIngredients] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
+
+    const addIngredient = (e) => {
+      e.preventDefault();
+      setIngredients([...ingredients, e.target[0].value]);
+      setSearchValue('');
+    }
+
+    const handleSearch = (e) => {
+        setSearchValue(e.target.value);
+    }
+
     return (
-        <p><HiOutlineSearch />I'm the search bar!</p>
+        <div className="search-bar-container">
+            <div className="search-bar">
+            <HiOutlineSearch />
+            {ingredients.map((ingredient, index) => (
+                <span className="ingredient" key={index}>{ingredient}</span>
+            ))}
+            <form onSubmit={addIngredient}>
+                <input value={searchValue} onChange={handleSearch} placeholder="Enter an ingredient . . ."></input>
+                {/* <button type='submit'>Add Ingredient</button> */}
+            </form>
+            </div>
+        </div>
+
     )
 }
 
