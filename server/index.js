da39d3d01242bc4e799e6480c857e58c2a0c95c0
Nathan Love <nlovephd@gmail.com>
2023-03-03 19:12:16 -0600

    reqs to /&q= queries api

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
+const port = 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/&q=*', (req, res) => {
+  console.log(process.env.API_URL, process.env)
+  axios.get(process.env.API_URL + req.url.slice(1))
+  .then(res => console.log(res));
 })
 
 app.listen(port, () => {
