f857b55695e579b5a791f1c69a49589716129d2f
SajanaB <sajanabalal@gmail.com>
2023-03-07 14:06:43 -0500

    Some changes

---
+++ src/Pages/ProfilePage/MoreDietRes.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import './MoreDietRes.css';
+
+const MoreDietaryRes = () => {
+
+  return (
+    <div >
+      <input className='inputContainer' id='auto' placeholder='add more restrictions'/>
+    </div>
+  );
+}
+
+export default MoreDietaryRes;
