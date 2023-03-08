b331ad13407a3006eb6e3fe3503ec48f4354c971
deirdre <deirdre.loyall@gmail.com>
2023-03-08 16:57:39 -0500

    i think this works? Nathan & Bryce would love to get your input on this

--- src/Components/NavBurger/NavBurger.jsx
+++ src/Components/NavBurger/NavBurger.jsx
@@ -10,6 +10,10 @@ const NavBurger = () => {
     navRef.current.classList.toggle("responsive_nav");
   }
 
+  const handleSignout = (e) => {
+    localStorage.removeItem('token');
+  }
+
   return (
     <header>
       <h3>LOGO</h3>
@@ -17,7 +21,7 @@ const NavBurger = () => {
         <Link to="/search">Home</Link>
         <Link to="/profile">Profile</Link>
         <Link to="/saved">Saved Recipes</Link>
-        <Link to="/login">Sign Out</Link>
+        <Link to="/login" onClick={handleSignout}>Sign Out</Link>
         <button className="nav-btn nav-close-btn" onClick={showNavbar}>
           <FaTimes />
         </button>
