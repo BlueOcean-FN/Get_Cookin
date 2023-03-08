ead468166ed7fe12a68fcaacf7a0759f3d3a9761
Nathan Love <nlovephd@gmail.com>
2023-03-08 15:43:09 -0600

    garbo

--- server/controllers/getAutocomplete.js
+++ server/controllers/getAutocomplete.js
@@ -1,12 +1,17 @@
 const axios = require('axios');
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 
 const getAutocomplete = (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Origin', '*');
+  console.log(process.env, '=================================')
   axios(`https://api.edamam.com/auto-complete?q=${req.query.search}&app_id=${process.env.FOOD_ID}&app_key=${process.env.FOOD_KEY}`)
   .then(results => {
-    res.send(JSON.stringify(results.data));
+    console.log(results.data);
+    res.send(JSON.stringify('there was a response!'));
   })
+  .catch(err => console.log('there was an error in getAutocomplete'));
 }
 
 module.exports = getAutocomplete;
--- server/controllers/recipeSearch.js
+++ server/controllers/recipeSearch.js
@@ -1,11 +1,14 @@
 const axios = require('axios');
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 
 const recipeSearch = (req, res) => {
+  console.log(process.env, '=================================');
   axios(`https://api.edamam.com/api/recipes/v2/?q=${req.query.q}&app_id=${process.env.RECIPE_ID}&app_key=${process.env.RECIPE_KEY}&type=public`)
   .then(results => {
     res.send(JSON.stringify(results.data));
   })
-
+  .catch(err => console.log('err happened'))
 }
 
 module.exports = recipeSearch;
