bf62f0b279af58390f8f0c6df9cf52c71f493fef
Bryce Anderson <abryce.anderson@gmail.com>
2023-03-09 11:43:12 -0500

    final changes on auth

--- server/index.js
+++ server/index.js
@@ -14,12 +14,14 @@ const { authenticateUser } = require('./middleware.js');
 const { addUser, findUser, getAutocomplete, getSaved, postSaved, recipeSearch } = require('./controllers');
 
 app.use(express.static(path.join(__dirname, '../dist')));
-app.use(cors());
+
 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 app.use(authenticateUser);
+
+
 
 
 // AUTHENTICATION  ===
