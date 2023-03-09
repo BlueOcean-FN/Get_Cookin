1424ca3af2bbebe927667de0333fc99e3f7e5d0c
andrewsittner <andrewsittner@gmail.com>
2023-03-09 14:49:43 -0600

    fix recipe search allowing multiple ing and multiple exclusions

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -13,7 +13,7 @@ const Card = ({ data }) => {
   const displayItem = (prop) => {
     let arr = [];
     if (!expanded || prop !== 'ingredients') for (let i = 0; i < 4 && i < data[prop].length; i++) arr.push(data[prop][i]);
-    else for (let i = 0; i < 5 && i < data[prop].length; i++) arr.push(data[prop][i]);
+    else for (let i = 0; i < 8 && i < data[prop].length; i++) arr.push(data[prop][i]);
     if (arr.length < data[prop].length) arr.push('...');
     return arr;
   }
