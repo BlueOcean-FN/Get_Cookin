4448fea518c6bf016ec680fbeacf54970a8835a7
Nathan Love <nlovephd@gmail.com>
2023-03-09 10:43:34 -0600

    honestly not sure whats diff here or how to check

--- server/index.js
+++ server/index.js
@@ -78,8 +78,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist' , 'index.html'));
 });
 
-
-
 app.listen(port, () => {
   console.log(`We are cookin' on port ${port}`)
 })
